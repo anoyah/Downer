c78ceecf4f66dafdd2652f488889b667ab9be13d
anoya <yother233@gmail.com>
2025-01-21 14:57:31 +0800

    test: downer_test.go update

--- downer_test.go
+++ downer_test.go
@@ -7,11 +7,15 @@ import (
 	"github.com/anoyah/downer/core"
 )
 
-func TestDownload(t *testing.T) {
+func TestLibrary(t *testing.T) {
+	wantedFilename := "./nginx-alpine-linux-arm64.tar.gz"
+
 	d, err := core.NewDp(&core.Config{
-		Arch:  "linux/arm64",
-		Name:  "nginx:alpine",
-		Proxy: "http://127.0.0.1:7890",
+		Arch:   "linux/arm64",
+		Name:   "nginx:alpine",
+		Proxy:  "http://127.0.0.1:7890",
+		Debug:  true,
+		Output: wantedFilename,
 	})
 	if err != nil {
 		t.Fatal(err)
@@ -21,7 +25,36 @@ func TestDownload(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	wantedFilename := "./nginx-alpine-linux-arm64.tar.gz"
+	t.Log("download success!")
+	fi, err := os.Stat(wantedFilename)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.Remove(wantedFilename)
+
+	if fi.Size() > 0 {
+		t.Log("finished")
+	}
+}
+
+func TestDownload(t *testing.T) {
+	wantedFilename := "./memos.tar.gz"
+
+	d, err := core.NewDp(&core.Config{
+		Arch:   "linux/amd64",
+		Name:   "neosmemo/memos:stable",
+		Proxy:  "http://127.0.0.1:7890",
+		Output: wantedFilename,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err = d.Run(); err != nil {
+		t.Fatal(err)
+	}
+
 	t.Log("download success!")
 	fi, err := os.Stat(wantedFilename)
 	if err != nil {
