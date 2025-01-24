7d91c454d1a4dc48177f2364a65500eb476aef33
anoya <yother233@gmail.com>
2025-01-24 11:35:56 +0800

    add: download hint

---
+++ tools/processbar.go
@@ -0,0 +1,35 @@
+package tools
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func ProgressBar(current, total int, desc string) {
+	if total < current {
+		return
+	}
+
+	percentage := float64(current) / float64(total) * 100
+	barLength := 20
+	arrow := ">"
+
+	if current == total {
+		arrow = ""
+		barLength += 1
+	}
+
+	filledLength := int(barLength * current / total)
+
+	completed := strings.Repeat("=", filledLength)
+	restOf := strings.Repeat(" ", barLength-filledLength)
+	bar := fmt.Sprintf("[%s%s%s]", completed, arrow, restOf)
+
+	std := os.Stdout
+
+	fmt.Fprintf(std, "\r%s: %s %.2f%%", desc, bar, percentage)
+	if current == total {
+		fmt.Fprintln(std)
+	}
+}
